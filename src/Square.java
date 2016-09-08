8ad80086302c1b8fe3af69b797950a44a5f35c64
Kevin Baxter <kbax6342@gmail.com>
2016-09-08 13:21:59 -0400

    Initial Commit

---
+++ src/Square.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+  
+public class Square {
+
+	public static void main(String[] args) {
+		
+	Scanner input = new Scanner(System.in);
+	
+	int number;
+		
+	System.out.println("Learn your squares and cubes1 \n");
+	
+	System.out.println("Enter and interger: ");
+		number = input.nextInt();
+		
+	System.out.println("Number \n ===");
+	
+	for( int i = number; i < 10 ; i++){
+		
+	}
+		
+	
+	System.out.println(number);
+	
+	}
+
+	
+
+}
