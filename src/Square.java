fdfb75f6fdff4993a0d2c461dc5970e889b4a463
Kevin Baxter <kbax6342@gmail.com>
2016-09-08 15:37:40 -0400

    Working App

--- src/Square.java
+++ src/Square.java
@@ -7,21 +7,42 @@ public class Square {
 		
 	Scanner input = new Scanner(System.in);
 	
-	int number;
-		
+	String number;
+	String loop;
+	
+	//Welcome the user to the App	
 	System.out.println("Learn your squares and cubes1 \n");
 	
+do{	
+	//Ask the user for an interger
 	System.out.println("Enter and interger: ");
-		number = input.nextInt();
-		
-	System.out.println("Number \n ===");
+		number = input.nextLine();
+		//changed the string to a number
+		 int x = Integer.parseInt(number);
 	
-	for( int i = number; i < 10 ; i++){
+	//Loop through the interget from the user
+	System.out.println("Number  " + "Squared  "  + "Cubed\n" + "====    " + "====     "+ "====");
+	
+	for( int i = 0; i <= x ; i++){
 		
+		System.out.println( i + "       " + i*i + "         " +  i*i*i);
 	}
+	
+	
 		
 	
-	System.out.println(number);
+//	System.out.println(number);
+	
+	
+
+
+	System.out.println("Continue(yes/no): ");
+		loop = input.nextLine();
+		
+		
+}
+while(loop.equalsIgnoreCase("yes"));
+	
 	
 	}
 
